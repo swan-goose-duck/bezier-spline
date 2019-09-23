7036df8f72c942cd9ecaa16e2525025e6d238283
swan.goose.duck <kaneres@bxscience.edu>
2019-09-22 23:58:18 -0400

    Revert "Update Main.java"
    This reverts commit 9a57cb64a9611af6210750a774006a3cbcb8b817.

--- Main.java
+++ Main.java
@@ -1,63 +1,35 @@
-import java.util.ArrayList;
 import java.io.*;
 
-import PathSmoother;
 import Point;
+import Spline;
 
-class Main {
+class Main {  
     public static void main(String[] args) {
-        //test points
-        int numPoints = 10;
-        Point currPos = new Point(0, 0);
-        double currHeading = Math.PI / 4;
-        Point finalPos = new Point(10, 10);
-        double finalHeading = 1.25 * Math.PI;
         
-        ArrayList<Point> Path = addPointsInLine(currPos, currHeading, numPoints);
-        Path.add(currPos);
-        Path.add(finalPos);
-        Path.addAll(addPointsInLine(finalPos, finalHeading, numPoints));
+        Point  currPos      = new Point (0, 0);
+        double currHeading  = 4;
+        Point  finalPos     = new Point (10, 20);
+        double finalHeading = 1;
+
+        Spline path = new Spline(currPos, currHeading, finalPos, finalHeading, 5);
         
-        for (Point point : Path) {
-            System.out.println(point.x + ", " + point.y);
+        String[][] point = new String[path.getPath().size()][2];
+        for (int i = 0; i < path.getPath().size(); i++) { 
+            point[i][0] = Double.toString(path.getPath().get(i).x); 
+            point[i][1] = Double.toString(path.getPath().get(i).y); 
         }
-
-        PathSmoother finalPath = new PathSmoother(Path, 20);
-        ArrayList<Point> last = finalPath.getFinalPath();
-
-        //create csv
-        String[][] point = new String[last.size()][2];
-        for (int i = 0; i < last.size(); i++) {
-            point[i][0] = Double.toString(last.get(i).x);
-            point[i][1] = Double.toString(last.get(i).y);
-        }
-
-        try {    
-            FileWriter fw = new FileWriter("test.csv");    
-            
-            for(int i = 0; i < point.length; i++){
-                fw.write(point[i][0] + "," + point[i][1] + "\n");       
+        
+        try { 
+            FileWriter fw = new FileWriter("test12.csv");
+        
+            for (int i = 0; i < point.length; i++) { 
+                fw.write(point[i][0] + "," + point[i][1] + "\n"); 
             }
+            fw.close(); 
+        } catch (Exception e) { System.out.println(e); }
 
-            fw.close();
-        } catch(Exception e) { System.out.println(e); }    
-           System.out.println("Success...");
-           
-      }
-
-    public static ArrayList<Point> addPointsInLine(Point point, double heading, int numPoints) {
-        ArrayList<Point> temp = new ArrayList<Point>();
-        double increment = 1 / numPoints;
-        double slope = Math.tan(heading);
-        double b = point.y - (slope * point.x);
-
-        if ((heading > Math.PI / 2) && (heading < 1.5 * Math.PI)) { increment *= -1; }
-        for (double i = 0; i <= 1; i += increment) {
-            double x = point.x * i ;
-            double y = slope * x + b;
-            Point temp1 = new Point(x, y);
-            temp.add(temp1);
-        }
-        return temp;
+        System.out.println("Success...");
+        
     }
+    
 }
